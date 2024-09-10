8dbdf3011bf7fd81e9b59ea9f2c1c781c8509320
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2024-09-10 10:29:50 -0500

    feat: change title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import AuthButton from "../components/AuthButton";
 
 export default function Home() {
   return (
-    <main className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Welcome to My App</h1>
+    <main>
+      <h1>Authentication TEST App</h1>
       <AuthButton />
     </main>
   );
